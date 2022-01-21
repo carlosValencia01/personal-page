9b031a03611bd187b6feb8ac64257498f6833248
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-01-20 21:04:03 -0700

    fix: added _redirects to fix netlify routes error and added a test page for routes

---
+++ src/components/TestPage.js
@@ -0,0 +1,7 @@
+export const TestPage = () => {
+  return (
+    <div>
+      <h1>This is a test page</h1>
+    </div>
+  );
+};
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { About } from "../components/personal/about/About";
+import { TestPage } from "../components/TestPage";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/about" element={<About />} />
+        <Route path="/test" element={<TestPage />} />
         <Route path="*" element={<About />} />
       </Routes>
     </BrowserRouter>
