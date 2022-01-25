338f931843ee5933911568f09983ff544a21fadd
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-01-24 17:37:02 -0700

    feat: style for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <footer className="footer__main">
-      <div className="container">
+      <div className="footer__content">
         <p> &copy; {year} Carlos Valencia</p>
       </div>
     </footer>
