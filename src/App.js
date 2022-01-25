046b5194760ca4d5dbb264b198846a8e0c24dd69
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-01-24 19:22:35 -0700

    feat: add responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Footer } from "./components/Footer";
+import { NavBar } from "./components/NavBar";
 import { AppRouter } from "./routers/AppRouter";
 
 function App() {
   return (
     <>
+      <NavBar />
       <AppRouter />
       <Footer />
     </>
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <nav className="navbar__main">
+      <ul className="navbar__list">
+        <li className="navbar__item">Home</li>
+        <li className="navbar__item">Blogs</li>
+        <li className="navbar__item">Apps</li>
+        <li className="navbar__item">API`s</li>
+        <li className="navbar__item">About</li>
+      </ul>
+    </nav>
+  );
+};
