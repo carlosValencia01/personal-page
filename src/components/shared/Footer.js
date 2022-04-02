7d742ad5fc23cb9140885190fa004fdfb98ba5bc
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-04-02 15:30:23 -0700

    feat: add mail option on footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,4 +1,4 @@
-// import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { IconButton } from "@mui/material";
@@ -17,6 +17,7 @@ export const Footer = () => {
         >
           <LinkedInIcon fontSize="inherit" />
         </IconButton>
+
         <IconButton
           aria-label="delete"
           size="large"
@@ -24,9 +25,14 @@ export const Footer = () => {
         >
           <GitHubIcon fontSize="inherit" />
         </IconButton>
-        {/* <IconButton aria-label="delete" size="large">
+
+        <IconButton
+          aria-label="delete"
+          size="large"
+          href="mailto:jocavalenciava@ittepic.edu.mx?Subject=Contact%20from%20web%20page"
+        >
           <MailOutlineIcon fontSize="inherit" />
-        </IconButton> */}
+        </IconButton>
       </div>
     </footer>
   );
