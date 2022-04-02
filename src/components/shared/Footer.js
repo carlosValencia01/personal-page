ad2590197cbc0611c7204b64fe2e51ae56626a7c
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-04-02 15:13:31 -0700

    feat: add links to footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,9 +1,32 @@
+// import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { IconButton } from "@mui/material";
+
 export const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <footer className="footer__main">
-      <div className="footer__content">
-        <p className="footer__text"> &copy; {year} Carlos Valencia</p>
+      <p className="footer__text">&copy; {year} Carlos Valencia</p>
+
+      <div className="footer__contact">
+        <IconButton
+          aria-label="delete"
+          size="large"
+          href="https://www.linkedin.com/in/carlos-valencia-valenzuela-534b0419a/"
+        >
+          <LinkedInIcon fontSize="inherit" />
+        </IconButton>
+        <IconButton
+          aria-label="delete"
+          size="large"
+          href="https://github.com/carlosValencia01"
+        >
+          <GitHubIcon fontSize="inherit" />
+        </IconButton>
+        {/* <IconButton aria-label="delete" size="large">
+          <MailOutlineIcon fontSize="inherit" />
+        </IconButton> */}
       </div>
     </footer>
   );
