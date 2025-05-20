c8146b0ea0845f18af6a4e8fab898d9f07a7ee16
Jose Carlos Valencia Valenzuela <carlosvalencia.dev@gmail.com>
2025-05-20 15:15:56 -0700

    feat: implementing holy grail

--- src/Pages/Blogs/BlogPost.jsx
+++ src/Pages/Blogs/BlogPost.jsx
@@ -1,6 +1,7 @@
 // import { useParams } from 'react-router-dom';
 import { useParams } from 'react-router';
 import blogs from '../../Data/blogs.json';
+import './style.css';
 
 function BlogPost() {
   const { slug } = useParams();
@@ -9,11 +10,26 @@ function BlogPost() {
   if (!blog) return <h2>Blog no encontrado</h2>;
 
   return (
-    <div className="max-w-3xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-2">{blog.title}</h1>
-      <p className="text-sm text-gray-600 mb-4">Por {blog.author} - {blog.date}</p>
-      <div className="prose">{blog.content}</div>
+    <div className="container">
+      <header>Mi Blog</header>
+      <nav>Navegación</nav>
+      <main>
+        <div className="max-w-3xl mx-auto p-4">
+           <h1 className="text-3xl font-bold mb-2">{blog.title}</h1>
+           <p className="text-sm text-gray-600 mb-4">Por {blog.author} - {blog.date}</p>
+           <div className="prose">{blog.content.map((x)=>(
+               <div key={x.subtitle}>
+                 <h2>{x.subtitle}</h2>
+                 <p>{x.contenido}</p>
+               </div>
+           )
+           )}</div>
+        </div>
+      </main>
+      <aside>Barra lateral</aside>
+      <footer>© 2025 Mi Nombre</footer>
     </div>
+
   );
 }
 
