6b71c87e26a28dda7a243d127eba2c2d0eccaa91
Jose Carlos Valencia Valenzuela <carlosvalencia.dev@gmail.com>
2025-06-04 16:32:49 -0700

    feat: add functionality to hangman

--- src/Apps/HangMan/HangmanMain.jsx
+++ src/Apps/HangMan/HangmanMain.jsx
@@ -1,12 +1,27 @@
 import './style.css';
 import { languages } from './languages';
 import { useState } from 'react';
+import clsx from 'clsx';
 
 
 export const HangmanMain = () => {
-    const [currentWord, setCurrentWord] = useState('react');
+    //State Values
+    const [currentWord, setCurrentWord] = useState('javascript');
+    const [guessedLettersArray, setGuessedLettersArray] = useState([]);
 
+    //Derived values
+    let wrongGuessCount = guessedLettersArray.filter(letter => !currentWord.includes(letter)).length;
+
+    //Static values
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    function handleGuess(letter){
+        setGuessedLettersArray(prev => 
+            prev.includes(letter)
+            ? prev
+            : [...prev, letter]);
+
+    }
 
     return (
         <div className="hangman-body">
@@ -19,22 +34,42 @@ export const HangmanMain = () => {
                 <p>Well done!</p>
             </section>
             <section className='hangman-languge-container'>
-                {languages.map(language => (
-                    <span style={{backgroundColor: language.backgroundColor, color:language.color}} key={language.name}>{language.name}</span>
+                {languages.map((language,i) => (
+                    <span 
+                        style={{backgroundColor: language.backgroundColor, color:language.color}} 
+                        key={language.name}
+                        className={i < wrongGuessCount ? 'lost':''}
+                        >{language.name}</span>
                 ))}
             </section>
             <section className='hangman-word-container'>
                 {
-                    currentWord.split("").map((letter, i)=>(
-                        <span key={i}>{letter}</span>
-                    ))
+                    currentWord.split("").map((letter, i)=>{                                                
+                        return <span  
+                                key={i}                                
+                                >
+                            {guessedLettersArray.includes(letter) ? letter : ''}</span>
+                    })
                 }
             </section>
             <section className='hangman-keyboard-container'>
                 {
-                    alphabet.split("").map((letter) => (
-                        <button key={letter}>{letter}</button>
-                    ))
+                    alphabet.split("").map((letter) => {
+                        const isGuessed = guessedLettersArray.includes(letter);
+                        const isCorrect = isGuessed && currentWord.split("").includes(letter);
+                        const isWrong = isGuessed && !currentWord.split("").includes(letter);
+                        const className = clsx({
+                            correct: isCorrect,
+                            wrong: isWrong
+                        });
+                        return(
+                            <button 
+                                className={className}
+                                key={letter} 
+                                onClick={()=>handleGuess(letter)}>{letter}</button>
+                        )
+                    }
+                )
                 }
             </section>
             <button className='hangman-newgame-button'>New Game</button>
