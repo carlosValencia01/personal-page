111311a2af7e12dc12f17fa43b80e287bbc096ac
Jose Carlos Valencia Valenzuela <carlosvalencia.dev@gmail.com>
2025-06-13 15:58:02 -0700

    fix:description on BlogCard

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -4,7 +4,7 @@ export const BlogCard = (props) => {
         <div className="blogcard-container">
             <img className='blogcard-image' src="https://static-blog.siteground.com/wp-content/uploads/sites/4/2022/02/what-is-a-blog-1200x600-1.jpeg" alt="" />
             <h3 className='blogcard-title'>{props.blog.title}</h3>
-            <h4>{props.blogs?.description }</h4> 
+            <h4>{props.blog.description }</h4> 
             <a className="blogcard-link-button" href={`/blog/${props.blog.slug}`}>Ir a</a>
         </div>
     )
